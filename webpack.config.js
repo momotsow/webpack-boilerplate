5897e44a2d65b3326ae92e27ca51df1a4da135a7
momotsow <kgomotsomkansi@gmail.com>
2022-11-03 13:50:26 +0200

    Add HTML Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  plugins: [new HtmlWebpackPlugin()],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
